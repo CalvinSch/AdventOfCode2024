407b7dc54a9588c0b5be7a36b00d21c1f91de6da
Calvin <60625109+CalvinSch@users.noreply.github.com>
2024-12-05 20:18:50 +0000

    Better late than never!

---
+++ day1.py
@@ -0,0 +1,16 @@
+left_list = []
+right_list = []
+total_distance = 0
+
+def process_input(filename: str): 
+    input = open("input.txt", "r")
+    #TODO: Split left and right columns into separate lists
+    #TODO: Sort lists
+
+def sum_list_distances(left_list: list[int], right_list: list[int]) -> int:
+    #TODO: For item in lists, abs val, total++
+    return 0
+
+if __name__ == "__main__":
+    process_input("input.txt")
+    sum_list_distances()
