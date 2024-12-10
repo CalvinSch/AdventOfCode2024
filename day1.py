f9e7a5231175713aa7d5b861566e895acd519aed
Calvin <60625109+CalvinSch@users.noreply.github.com>
2024-12-10 16:10:50 +0000

    day1 p1 done!

--- day1.py
+++ day1.py
@@ -1,16 +1,29 @@
+import math
+
 left_list = []
 right_list = []
-total_distance = 0
 
-def process_input(filename: str): 
-    input = open("input.txt", "r")
-    #TODO: Split left and right columns into separate lists
-    #TODO: Sort lists
+def process_location_id_list(filename: str): 
+    with open("day1_input.txt", "r") as input:
+        for line in input:
+            #Split Location IDs
+            left_list_entry, right_list_entry = line.split('   ')
+
+            #Cast to int and append to lists
+            left_list.append(int(left_list_entry))
+            right_list.append(int(right_list_entry))
+    
+    #Sort to help with comparison between smallest -> largest
+    left_list.sort()
+    right_list.sort()
 
 def sum_list_distances(left_list: list[int], right_list: list[int]) -> int:
-    #TODO: For item in lists, abs val, total++
-    return 0
+    total_distance = 0
+    if(len(left_list) == len(right_list)):
+        for i in range(len(left_list)):
+            total_distance += abs(left_list[i] - right_list[i])
+    return total_distance
 
 if __name__ == "__main__":
-    process_input("input.txt")
-    sum_list_distances()
+    process_location_id_list("input.txt")
+    print(sum_list_distances(left_list, right_list))
